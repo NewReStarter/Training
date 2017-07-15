486dc9ecf8b36d66d75ce7105d62baf7e1bc38dc
GAO Ziliu Lawrence <ziliugao@gmail.com>
2017-07-15 10:37:46 +0800

    Add files via upload
    the idea is the same with palsquare.cpp

---
+++ USACO/Section_1/dualpal.cpp
@@ -0,0 +1,81 @@
+/*
+ID: ziliuga1
+LANG: C++11
+TASK: dualpal
+*/
+
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <string>
+using namespace std;
+
+int base;
+string trans(int x);
+bool ispal(string s);
+string r(string s);
+int main()
+{
+	freopen("dualpal.in","r",stdin);
+	freopen("dualpal.out","w",stdout);
+	int n,s;
+	int flag;
+	cin>>n>>s;
+	int count = 0;
+	while(count < n)
+	{
+		++s;
+		for(base=2;base<=10;base++)
+		{
+			if(ispal(trans(s)))
+				++flag;
+		}
+		if(flag>=2)
+		{
+			cout<<s<<endl;
+			++count;
+		}
+		flag=0;
+	}
+	return 0;
+
+}
+
+string trans(int x)
+{
+	string s;
+	int d,m;
+	while(x>=base)
+	{
+		m = x%base;
+		if(m<10) s+=(char)(m+48);
+		else s+=(char)(m+55);
+		x/=base;
+	}
+	if(x<10) s+=(char)(x+48);
+	else s+=(char)(x+55);
+
+	return r(s);
+}
+
+inline bool ispal(string s)
+{
+	int last=s.length()-1;
+	int first=0;
+	while(last>=first)
+	{
+		if(s[last]!=s[first])
+			return 0;
+		last--;
+		first++;
+	}
+	return 1;
+}
+
+string r(string s)
+{
+	string tmp;
+	for(int i=s.length()-1; i>=0;i--)
+		tmp+=s[i];
+	return tmp;
+}
